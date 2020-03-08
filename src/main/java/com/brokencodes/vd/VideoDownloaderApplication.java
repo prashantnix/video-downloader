c9b36e73448ba00bd8b47b8f5440fd758e2f092d
Prashant Chaturvedi <prashant.nixsys@gmail.com>
2020-03-08 07:11:37 +0530

    Sets up project - spring boot + angular

---
+++ src/main/java/com/brokencodes/vd/VideoDownloaderApplication.java
@@ -0,0 +1,13 @@
+package com.brokencodes.vd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoDownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoDownloaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brokencodes/vd/VideoDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brokencodes.vd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoDownloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
