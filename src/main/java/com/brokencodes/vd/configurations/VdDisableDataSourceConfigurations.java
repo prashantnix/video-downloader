a2cbaeb745fa4e972e6f158b92e9b3dd454cb634
Prashant Chaturvedi <prashant.nixsys@gmail.com>
2020-03-08 12:17:52 +0530

    Adds property to disable security and datasource autoconfiguration for testing

---
+++ src/main/java/com/brokencodes/vd/configurations/VdDisableDataSourceConfigurations.java
@@ -0,0 +1,19 @@
+package com.brokencodes.vd.configurations;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Setting the value of the property: vd.features.disable-data-source to true activates this configuration
+ * and disables all connections to the data source.
+ */
+@Configuration
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+@ConditionalOnProperty(
+        name = "vd.features.disable-data-source",
+        havingValue = "true"
+)
+public class VdDisableDataSourceConfigurations {
+}
---
+++ src/main/java/com/brokencodes/vd/configurations/VdDisableSecurityConfigurations.java
@@ -0,0 +1,19 @@
+package com.brokencodes.vd.configurations;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Setting the value of the property: vd.features.disable-security to true activates this configuration
+ * and disables all the security features.
+ */
+@Configuration
+@EnableAutoConfiguration(exclude = SecurityAutoConfiguration.class)
+@ConditionalOnProperty(
+        name = "vd.features.disable-security",
+        havingValue = "true"
+)
+public class VdDisableSecurityConfigurations {
+}
