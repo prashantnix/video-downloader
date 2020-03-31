239351ed79230511ec44a2dfd706df73ead4ca5d
Prashant Chaturvedi <prashant.nixsys@gmail.com>
2020-03-31 21:23:13 +0530

    Formats code

--- src/test/java/com/brokencodes/vd/VideoDownloaderApplicationTests.java
+++ src/test/java/com/brokencodes/vd/VideoDownloaderApplicationTests.java
@@ -1,13 +1,13 @@
 package com.brokencodes.vd;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ContextConfiguration(
+        classes = VideoDownloaderApplication.class,
+        loader = SpringBootContextLoader.class
+)
 class VideoDownloaderApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
