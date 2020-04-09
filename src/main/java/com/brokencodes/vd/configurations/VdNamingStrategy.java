7a7da29ef4a3a95e254fc9bdecb65f7401f77b7e
Prashant Chaturvedi <prashant.nixsys@gmail.com>
2020-04-09 22:34:40 +0530

    Reformats code

--- src/main/java/com/brokencodes/vd/configurations/VdNamingStrategy.java
+++ src/main/java/com/brokencodes/vd/configurations/VdNamingStrategy.java
@@ -4,13 +4,22 @@ import org.hibernate.boot.model.naming.Identifier;
 import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
 import org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy;
 
+import java.text.MessageFormat;
+
+/**
+ * Adds vd_ as prefix for all table names.
+ */
 public class VdNamingStrategy extends SpringPhysicalNamingStrategy {
 
-    private static final String PREFIX = "vd_";
+    private static final String TABLE_NAME_FORMAT = "vd_{0}";
 
     @Override
     public Identifier toPhysicalTableName(Identifier name, JdbcEnvironment jdbcEnvironment) {
-        return new Identifier(PREFIX + super.toPhysicalTableName(name, jdbcEnvironment).getText(), name.isQuoted());
+        return new Identifier(
+                MessageFormat.format(
+                        TABLE_NAME_FORMAT,
+                        super.toPhysicalTableName(name, jdbcEnvironment).getText()),
+                name.isQuoted());
     }
 
 }
