86f4a4d423eeea7a98b99023b5755d2bb7c4229b
Prashant Chaturvedi <prashant.nixsys@gmail.com>
2020-04-12 20:22:21 +0530

    Fixes NPE if all validations passed on request bean

--- src/main/java/com/brokencodes/vd/aspects/HttpRequestBodyValidatingAspect.java
+++ src/main/java/com/brokencodes/vd/aspects/HttpRequestBodyValidatingAspect.java
@@ -2,7 +2,6 @@ package com.brokencodes.vd.aspects;
 
 import com.brokencodes.vd.annotations.ValidateRequestBody;
 import com.brokencodes.vd.endpoints.base.IValidateRequest;
-import com.brokencodes.vd.endpoints.base.Validation;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -11,6 +10,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 @Aspect
 @Component
@@ -22,7 +22,8 @@ public class HttpRequestBodyValidatingAspect {
                 .filter(arg -> arg instanceof IValidateRequest)
                 .map(arg -> (IValidateRequest) arg)
                 .map(IValidateRequest::validate)
-                .filter(Validation::isFailed)
+                .filter(Objects::nonNull)
+                .filter(validation -> validation.isFailed())
                 .findFirst()
                 .ifPresent(failedValidation -> { throw new ResponseStatusException(HttpStatus.BAD_REQUEST, failedValidation.getMessage()); });
     }
